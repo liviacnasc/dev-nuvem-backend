4c054fce6c5a7160f291455661cc5d82d1908c97
livia <120498547+liviacnasc@users.noreply.github.com>
2025-03-04 18:20:26 +0000

    add mongo setup

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import { Mongo } from "./src/database/mongo.js";
+import { config } from "dotenv";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+config();
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+async function main() {
+    const port = 3000;
+
+    const app = express();
+    const mongoConnection = await Mongo.connect({mongoConnectionString: process.env.MONGO_CONNECTION_STRING, mongoDbName: process.env.MONGO_DATABASE_NAME});
+    app.use(express.json());
+
+    app.get("/", (req, res) => {
+        res.send({
+            success: true,
+            statusCode: 200
+        });
+    });
+
+    // app.use('/daily', desafioRoute)
+
+    app.listen(port, function(err) {
+        if(err) console.log("Error in server setup");
+
+        console.log(`Servidor conectado à porta ${port}`);
+    })
+}
+
+main();
